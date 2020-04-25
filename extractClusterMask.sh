de6ca444dd0dda5dae28c75f8e03b79380b1198a
soffiafdz <so1.618e@gmail.com>
2020-04-25 01:17:22 -0700

    fixed bug in conditionals, format and variables. Ready for Test.

--- extractClusterMask.sh
+++ extractClusterMask.sh
@@ -6,73 +6,71 @@
 # © 2020 Sofía Fernández, M.Sc. | so1.618e@gmail.com
 ######################################################################
 
-## Functions for help, usage, and errors.
 usage(){
-    printf "Usage: %s [-h] <-i IMAGE> <-n NUMBER>\n" "$0";
+  printf "Usage: %s [-h] -i <Image> -n <NUMBER>\n" "$0"
 }
 
 help_msj(){
-    usage;
-    printf "
-    -i\tPath to the Cluster Index image.
-    \t\tThe clusters must be assigned a unique number (from 1 to N).
-    \t\tThis can be the output of fsl's cluster.
-    -n\tNumber of clusters to extract.
-    -h\tDisplay this help and exit.\n"
+  usage
+  printf "
+  COMPULSORY ARGUMENTS
+  -i\tPath to the Cluster Index Image.
+  \t\tThe clusters must be assigned a unique number (from 1 to N).
+  \t\tThis can be the output of fsl's cluster.
+  -n\tNumber of clusters to extract.
+  OPTIONAL ARGUMENTS
+  -h\tDisplay this help and exit.\n"
+  exit
 }
 
-exit_error(){
-   usage;
-   exit 1;
+err(){
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]:" >&2
+  printf $* >&2
+  exit 1
 }
+
+main(){
+  ## If no arguments, show help and exit
+  [[ $# -eq 0 ]] && help_msj
+
+## Check FSL is installed and fslmaths executable
+fslmaths &>/dev/null \
+  || err "fslmaths is not found in PATH. Check FSL installation"
 
 ## Argument parser
-while getopts "hi:n:" OPT; do
-    case "$OPT" in
-        h) help_msj;
-            exit;;
-        i) [ -f $OPTARG ] \
-                || printf "ERROR: %s is not a file.\n" "$OPTARG" >&2 \
-                && exit_error;
-            [ -e $OPTARG ] \
-                || printf "ERROR: Could not find %s.\n" "$OPTARG" >&2 \
-                && exit_error;
-            IMAGE=$OPTARG;;
-        n) [ $NUM =~ '^[0-9]+$' ] \
-                || printf "ERROR: %s must be a positive whole number.\n" \
-                    "$OPTARG" >&2 \
-                && exit_error;
-            [ $NUM -eq "0" ] \
-                && printf "ERROR: %s must be greater than zero.\n" \
-                    "$OPTARG" >&2 \
-                && exit_error;
-                NUM=$OPTARG;;
-        :) printf "Missing argument for -%s.\n" "$OPTARG" >&2;
-            exit_error;;
-        ?) printf "Illegal option: %s.\n" "$OPTARG" >&2;
-            exit_error;;
-    esac
+while getopts "hi:n:" arg; do
+  case "$arg" in
+    h) help_msj;;
+    :) err "Missing argument for -%s.\n" "$OPTARG";;
+    ?) err "Illegal option: %s.\n" "$OPTARG";;
+    i)
+      [[ -f "$OPTARG" ]] || err "%s is not a file.\n" "$OPTARG"
+      [[ -e "$OPTARG" ]] || err "%s not found.\n" "$OPTARG"
+      Image="$OPTARG"
+      ;;
+    n)
+      [[ "$OPTARG" -gt 0 ]] 2>/dev/null \
+        || err "%s must be natural number (>0).\n" "$OPTARG"
+      Num=$OPTARG
+      ;;
+  esac
 done
 
-## Main
-
 # Derivated variables
-EXT=${IMAGE#*.};
-BNAME=$(basename "$IMAGE" "$EXT");
-OUTDIR="${BNAME}_masks";
+ext=${Image#*.};
+BName=$(basename "$Image" "$ext");
+Outdir="${BName}_masks";
 
-# Create directory for outputs with same name as the input IMAGE
-mkdir "$OUTDIR";
+# Create directory for outputs with same name as the input image
+mkdir "$Outdir";
 
 # Loop through the number of clusters requested in NUM.
 # Implementation of fslmaths extracting the indexed cluster.
 
-for INDEX in {1.."$NUM"}; do
-    OUTPUT="${OUTDIR}/${BNAME}_$INDEX"
-    fslmaths \
-        -dt int \
-        -thr "$INDEX" \
-        -uthr "$INDEX" \
-        -bin \
-        "$OUTPUT";
-    done
+for idx in {1.."$NUM"}; do
+  Out="${Outdir}/${BName}_$idx"
+  fslmaths -dt int "$Image" -thr "$idx" -uthr "$idx" -bin "$Out";
+done
+}
+
+main "$@"
